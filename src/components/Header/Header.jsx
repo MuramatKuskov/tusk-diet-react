1995fd253e06ef4835cc4530027c1736856943a6
Kuskov <ki6org1488@gmail.com>
2023-09-11 10:43:42 +0300

    redesigned

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,68 +1,30 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import RecipeForm from '../../pages/RecipeForm/RecipeForm';
-import Landing from '../../pages/Landing/Landing';
 import Index from '../../pages/Index/Index';
 import Search from '../../pages/Search/Search';
 import ShoppingList from '../../pages/ShoppingList/ShoppingList';
 import { PageNavContext } from '../../context';
-import Roadmap from '../../pages/Roadmap/Roadmap';
 
 const Header = () => {
-	const [sideBarStatus, setSideBarStatus] = useState(false);
 	const { currentPage, setCurrentPage } = useContext(PageNavContext);
 
-	function toggleSideBar() {
-		setSideBarStatus(sideBarStatus => sideBarStatus = !sideBarStatus);
-	}
-
-	function handleNav(e) {
-		switch (e.target.value) {
-			case 0:
-				setCurrentPage(<Index />);
-				break;
-			case 1:
-				setCurrentPage(<Search query="" />);
-				break;
-			case 2:
-				setCurrentPage(<RecipeForm />);
-				break;
-			case 3:
-				setCurrentPage(<ShoppingList />);
-				break;
-			case 4:
-				setCurrentPage(<Roadmap />);
-				break;
-			case 5:
-				setCurrentPage(<Landing />);
-				break;
-		}
-		toggleSideBar();
-	}
-
 	return (
-		<header className='header'>
-			<h1 className='covering'>Рацион Бивня</h1>
-			<div className='burger' onClick={toggleSideBar}>
-				<div className="burger-line"></div>
-				<div className="burger-line"></div>
-				<div className="burger-line"></div>
-			</div>
-			<div className={['sidebar', sideBarStatus === true ? 'sidebar-active' : ''].join(' ')}>
-				<div className="sidebar-close" onClick={toggleSideBar}>
-					<div className="sidebar-close-line"></div>
-					<div className="sidebar-close-line"></div>
+		<>
+			<header className='header'>
+				<div className="header-row">
+					<h1 className='covering'>Рацион Бивня</h1>
+					<div className="header-user">
+						<img onClick={() => setCurrentPage(<ShoppingList />)} src="assets/shopping-cart.svg" alt="список покупок" className="ico-btn" />
+					</div>
 				</div>
-				<ol className="sidebar-list">
-					<li className="sidebar-link" onClick={handleNav} value={0}>Главная</li>
-					<li className="sidebar-link" onClick={handleNav} value={1}>Поиск рецептов</li>
-					<li className="sidebar-link" onClick={handleNav} value={2}>Добавить рецепт</li>
-					<li className="sidebar-link" onClick={handleNav} value={3}>Список покупок</li>
-					<li className="sidebar-link" onClick={handleNav} value={4}>Дорожная карта</li>
-					<li className="sidebar-link" onClick={handleNav} value={5}>Описание</li>
-				</ol>
-			</div>
-		</header>
+			</header>
+			<nav className="navbar">
+				<div className="navbar-item" onClick={() => setCurrentPage(<Index />)}>Главная</div>
+				<div className="navbar-item" onClick={() => setCurrentPage(<Search query="" />)}>Найти рецепт</div>
+				<div className="navbar-item" onClick={() => setCurrentPage(<RecipeForm />)}>Добавить рецепт</div>
+			</nav>
+		</>
 	);
 };
 
