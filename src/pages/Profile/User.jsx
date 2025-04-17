506e446c0bc8754565084c944a3d46ed2b6775f1
Kuskov <ki6org1488@gmail.com>
2025-04-17 20:51:44 +0300

    removed import of non-existing component

--- src/pages/Profile/User.jsx
+++ src/pages/Profile/User.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './User.css';
-import EditableList from '../../UI/EditableList/EditableList';
 
 const User = (props) => {
 	return (
 		<div className='page page-user user'>
-			<EditableList />
 		</div>
 	);
 };
